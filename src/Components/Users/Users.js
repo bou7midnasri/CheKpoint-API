e1279b97d8fa21fe84ca7d8bc589199dfa46240d
unknown <user@MAINTENANCE3.scarthage.local>
2021-02-22 13:30:35 +0100

    API

---
+++ src/Components/Users/Users.js
@@ -0,0 +1,35 @@
+import  { useState, useEffect } from 'react'
+import axios from "axios";
+import './Users.css';
+
+const Users = () => {
+    const [users, setusers] = useState([]);
+
+    const getUsers =async()=>{
+        try {
+            const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+            setusers(res.data);
+        } catch (err) {
+            console.log("Error", err)
+        }
+        
+    };
+  useEffect(() => {
+    getUsers();
+  }, []);
+    return (
+        <div>
+            <p>
+                {users.map((user, key) => (
+                    <div className="container">
+                     <span key={key}>{user.name}</span>
+                    </div>
+                )
+
+                )}
+            </p>
+        </div>
+    );
+};
+
+export default Users
